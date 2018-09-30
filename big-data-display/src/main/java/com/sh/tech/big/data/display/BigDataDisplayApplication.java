6b28666f24847e8f2a211ea006d0f4a194a80737
feifan.gou <feifan.gou@gmail.com>
2018-09-30 18:42:22 +0800

    init project

---
+++ big-data-display/src/main/java/com/sh/tech/big/data/display/BigDataDisplayApplication.java
@@ -0,0 +1,12 @@
+package com.sh.tech.big.data.display;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataDisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDataDisplayApplication.class, args);
+	}
+}
---
+++ data-statistics-feign/src/main/java/com/sh/tech/feign/TestService.java
@@ -0,0 +1,10 @@
+package com.sh.tech.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * Created by feifan.gou@gmail.com on 2018/9/30 18:32.
+ */
+@FeignClient("")
+public interface TestService {
+}
